94e916eebd3e89cdb7434e05bc5ef101befb32b1
TmgGems <gyalwanglama123@gmail.com>
2023-08-02 14:17:55 +0545

    Done with list

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,18 @@ import Title from"./components/Title";
 import Example from"./components/Example";
 import Goal from"./components/Goal";
 import Garage from "./components/Garage";
+import Ternary from "./components/ternaryoperator";
+import LIST from "./components/list";
 const App=()=>{
     const cars = ['BMW'];
     return <>
     <Title name ="TODO App"/>
-    <Example />
+     <Example />
     <Goal isGoal={true}/>
     <Garage cars={cars}/>
+    <Ternary/>
+    <LIST/> 
+    
     </>
 
 };
---
+++ client/src/components/ternaryoperator.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ternaryoperator() {
+  return (
+    <div>ternaryoperator</div>
+  )
+}
+
+export default ternaryoperator;
