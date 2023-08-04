a54129ea787c42b4339e32a6bfc47519eb308106
TmgGems <gyalwanglama123@gmail.com>
2023-08-04 14:32:05 +0545

    Not running

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import { Col,Container, Row } from "react-bootstrap";
-import Title from"./components/Title";
+import { Col, Container, Row } from "react-bootstrap";
+import Title from "./components/Title";
 import Todo from "./pages/Todo";
 import AddTodo from "./components/AddTodo";
-import {API_SERVER} from "./constants";
+import { API_SERVER } from "./contexts";
 
-const App=()=>{
-    return (
+const App = () => {
+  return (
     <>
-        <Container>
+      <Container>
         <Row>
-            <Col md={{span: 6, offset: 3}}>
-              <Title name = "TODO App"/>
-              <AddTodo label = "Add New Task" placeholder = "Eg: Do Homework" url = {`${API_SERVER}/todos`} 
-              />
-              <Todo />
-            </Col>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Title name="TODO App" />
+            <AddTodo
+              label="Add New Task"
+              placeholder="Eg: Do Homework"
+              url={`${API_SERVER}/todos`}
+            />
+            <Todo />
+          </Col>
         </Row>
-
       </Container>
     </>
-    );
-}
+  );
+};
 
 export default App;
