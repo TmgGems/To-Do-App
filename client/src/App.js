b91b3a3006809b32768b90ba30747c03a78fa85d
TmgGems <gyalwanglama123@gmail.com>
2023-08-04 13:11:17 +0545

    Final Day of Todo

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import { Col,Container, Row } from "react-bootstrap";
 import Title from"./components/Title";
+import Todo from "./pages/Todo";
 import AddTodo from "./components/AddTodo";
+import {API_SERVER} from "./constants";
+
 const App=()=>{
-    return <>
+    return (
+    <>
         <Container>
         <Row>
             <Col md={{span: 6, offset: 3}}>
               <Title name = "TODO App"/>
-              <AddTodo label = "Add New Task" placeholder = "Eg: Do Homework" />
+              <AddTodo label = "Add New Task" placeholder = "Eg: Do Homework" url = {`${API_SERVER}/todos`} 
+              />
+              <Todo />
             </Col>
         </Row>
 
       </Container>
     </>
-
-};
+    );
+}
 
 export default App;
